90959c08337043fd8b3e78b8d38a84d35f94c364
Patricio Herrera <rapidoherrera@gmail.com>
2020-12-05 10:26:26 -0400

    se agrego el personalizador de administardor

--- Magicards/Web/Web/settings.py
+++ Magicards/Web/Web/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_interface',
+    'colorfield',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +44,8 @@ INSTALLED_APPS = [
     'apps.Producto',
     'apps.Usuario',
 ]
+
+X_FRAME_OPTIONS='SAMEORIGIN'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
