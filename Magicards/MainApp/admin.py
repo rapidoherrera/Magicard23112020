29e104a5a8ecc0c38f89a77740be2f5a1d221701
Patricio Herrera <rapidoherrera@gmail.com>
2020-12-05 07:28:30 -0400

    elimino carpeta antigua MainApp del Trabajo, por obsoleta

--- Magicards/MainApp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Persona
-from .models import Producto
-
-admin.site.register(Persona)
-admin.site.register(Producto)
-
-
--- Magicards/MainApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-from django.contrib import admin
-
-urlpatterns = [
-    path('', views.inicio, name='inicio'),
-    path('conocenos', views.quienessomos, name='conocenos'),    
-    path('registro', views.nuevousuario, name='nuevo'),
-]
